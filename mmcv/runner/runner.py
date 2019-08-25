52a62617af5b562256641beff749c7e677b32053
duanhaodong <duanhaodong@sensetime.com>
2019-08-25 10:19:47 +0800

    multi loader bug fix

--- mmcv/runner/runner.py
+++ mmcv/runner/runner.py
@@ -276,7 +276,7 @@ class Runner(object):
                 self._inner_iter = i
                 self.call_hook('before_train_iter')
                 outputs = self.batch_processor(
-                    self.model, data, train_mode=True, source='', **kwargs)
+                    self.model, data_batch, train_mode=True, source='', **kwargs)
                 if not isinstance(outputs, dict):
                     raise TypeError('batch_processor() must return a dict')
                 if 'log_vars' in outputs:
@@ -285,19 +285,19 @@ class Runner(object):
                 self.call_hook('after_train_iter')
 
                 auxiliary_iter_times = [1] * len(auxiliary_data_iters)
-                for idx, it, nt in enumerate(zip(auxiliary_data_iters, auxiliary_iter_times)):
+                for idx, pair in enumerate(zip(auxiliary_data_iters, auxiliary_iter_times)):
+                    it, nt = pair
                     for step in range(nt):
                         data_batch = next(it)
                         self.call_hook('before_train_iter')
                         outputs = self.batch_processor(
-                            self.model, data, train_mode=True, source='/aux' + str(idx), **kwargs)
+                            self.model, data_batch, train_mode=True, source='/aux' + str(idx), **kwargs)
                         if not isinstance(outputs, dict):
                             raise TypeError('batch_processor() must return a dict')
                         if 'log_vars' in outputs:
                             self.log_buffer.update(outputs['log_vars'], outputs['num_samples'])
                         self.outputs = outputs
                         self.call_hook('after_train_iter')
-                        
                 self._iter += 1
 
         else:
