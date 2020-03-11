f2da882b442e076ca1f6c4ccbbb96c21c7de71f5
duanhaodong <duanhaodong@sensetime.com>
2020-03-11 23:06:30 +0800

    fix wrong import

--- mmcv/runner/hooks/logger/base.py
+++ mmcv/runner/hooks/logger/base.py
@@ -2,7 +2,8 @@
 from abc import ABCMeta, abstractmethod
 
 from ..hook import Hook
-from ...utils import get_dist_info, get_host_info, get_time_str, obj_from_dict
+from ...utils import get_host_info, get_time_str, obj_from_dict
+from ...dist_utils import get_dist_info
 import torch.distributed as dist
 import torch
 
