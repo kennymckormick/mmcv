af33eef0676e35aeec9366ad3e7e450f6f7c76b7
duanhaodong <duanhaodong@sensetime.com>
2020-03-12 16:41:56 +0800

    add missing import

--- mmcv/parallel/__init__.py
+++ mmcv/parallel/__init__.py
@@ -3,9 +3,10 @@ from .collate import collate
 from .data_container import DataContainer
 from .data_parallel import MMDataParallel
 from .distributed import MMDistributedDataParallel
+from .distributed import MMAPEXDistributedDataParallel
 from .scatter_gather import scatter, scatter_kwargs
 
 __all__ = [
     'collate', 'DataContainer', 'MMDataParallel', 'MMDistributedDataParallel',
-    'scatter', 'scatter_kwargs'
+    'scatter', 'scatter_kwargs', 'MMAPEXDistributedDataParallel'
 ]
