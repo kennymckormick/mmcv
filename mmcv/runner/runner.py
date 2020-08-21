291af883625a4f4df693f7c599952d7b4b55769f
HaodongDuan <duanhaodong@sensetime.com>
2020-08-21 09:32:32 +0800

    bug fix

--- mmcv/runner/runner.py
+++ mmcv/runner/runner.py
@@ -223,12 +223,12 @@ class Runner(object):
             if 'bn_nowd' in optimizer and optimizer['bn_nowd']:
                 for k in params.keys():
                     if len(params[k]):
-                        optim_params.append(dict(info[k].items() + [('params': params[k])]))
+                        optim_params.append(dict(info[k].items() + [('params', params[k])]))
             else:
                 optim_params.append({'params': params['res'] + params['bn']})
                 for k in params.keys():
                     if k not in ['res', 'bn'] and len(params[k]):
-                        optim_params.append(dict(info[k].items() + [('params': params[k])]))
+                        optim_params.append(dict(info[k].items() + [('params', params[k])]))
 
             if 'bn_nowd' in optimizer:
                 optimizer.pop('bn_nowd')
